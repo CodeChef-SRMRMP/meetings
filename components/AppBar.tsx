5548bd8b60fd88b240cc35616f9f139bd01aa654
Chanakyha <vetrichanakyha2003@gmail.com>
2022-11-26 14:12:15 +0530

    Front Page is Done

---
+++ components/AppBar.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
+import SecurityIcon from "@mui/icons-material/Security";
+import JavascriptIcon from "@mui/icons-material/Javascript";
+import TerminalIcon from "@mui/icons-material/Terminal";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import SourceIcon from "@mui/icons-material/Source";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Drawer,
+  Button,
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  List,
+  ListItem,
+  IconButton,
+  ListItemButton,
+  ListSubheader,
+  ListItemIcon,
+} from "@mui/material";
+const menuItems = [
+  { name: "Family Meet", icon: <Diversity3Icon /> },
+  { name: "Web3 Meet", icon: <CurrencyBitcoinIcon /> },
+  { name: "Cyber Security Meet", icon: <SecurityIcon /> },
+  { name: "Development Meet", icon: <JavascriptIcon /> },
+  { name: "CP Meet", icon: <TerminalIcon /> },
+  { name: "PR&M Meet", icon: <ManageAccountsIcon /> },
+  { name: "Creatives Meet", icon: <DesignServicesIcon /> },
+  { name: "Content Meet", icon: <SourceIcon /> },
+];
+const adminMenu = [
+  { name: "Create Meet", icon: <AddCircleOutlineIcon /> },
+  { name: "Delete a Meet", icon: <DeleteIcon /> },
+];
+export default function NavBar() {
+  const [drawerOpen, setIsDrawerOpen] = useState(false);
+  const admin = true;
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={() => setIsDrawerOpen(true)}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              CodeChef SRMRMP Meet
+            </Typography>
+            <Button color="inherit">Admin Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer open={drawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        <List>
+          {admin && (
+            <>
+              <ListSubheader component="div">Admin Panel</ListSubheader>
+              {adminMenu?.map(({ name, icon }, key) => (
+                <ListItem key={key} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{icon}</ListItemIcon>
+                    <p className="font-semibold">{name}</p>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </>
+          )}
+          <ListSubheader component="div">Meetings</ListSubheader>
+          {menuItems?.map(({ name, icon }, key) => (
+            <ListItem key={key} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{icon}</ListItemIcon>
+                <p className="font-semibold">{name}</p>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import AppBar from "../components/AppBar";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center text-center px-10 py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col md:scale-150 items-center space-y-5 justify-center">
-        <Image
-          src={require("../assets/CodeChef-logo.png")}
-          alt="CodeChef Logo"
-          className="w-32 h-32 hover:animate-spin md:cursor-pointer"
-        />
-        <p className="font-bold text-2xl w-96 animate-pulse animate-bounce">
-          Use Specific Domain Route to navigate to the Meet
-        </p>
-      </div>
+      <header>
+        <AppBar />
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-center text-center px-10 py-2">
+        <div className="flex flex-col md:scale-150 items-center space-y-5 justify-center">
+          <Image
+            src={require("../assets/CodeChef-logo.png")}
+            alt="CodeChef Logo"
+            className="w-32 h-32 hover:animate-spin md:cursor-pointer"
+          />
+          <p className="font-bold text-2xl w-96 animate-pulse animate-bounce">
+            Use Specific Domain Route to navigate to the Meet
+          </p>
+        </div>
+      </main>
     </div>
   );
 };
